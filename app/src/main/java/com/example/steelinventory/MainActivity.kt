95569956ab0620876bc1e41de4604b833e726371
gritenlips-eng <gritenlips@gmail.com>
2026-08-08 10:21:39 +0330

    Update MainActivity.kt

--- app/src/main/java/com/example/steelinventory/MainActivity.kt
+++ app/src/main/java/com/example/steelinventory/MainActivity.kt
@@ -25,7 +25,9 @@ class MainActivity : ComponentActivity() {
                     composable("factory_products") { FactoryProductsScreen(db.inventoryDao()) }
                     composable("product_report") { ProductReportScreen(db.inventoryDao()) }
                     composable("channel_spec") { ChannelSpecScreen(db.channelSpecDao()) }
-                    composable("settings") { SettingsScreen(db) }
+                    composable("settings") {
+                        SettingsScreen(onBack = { nav.popBackStack() })
+                    }
                 }
             }
         }
