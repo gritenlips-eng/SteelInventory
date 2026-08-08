570a27e7ab5cac3866699a8b5307520f24290727
gritenlips-eng <gritenlips@gmail.com>
2026-08-08 09:06:45 +0330

    Update SettingsScreen.kt

--- app/src/main/java/com/example/steelinventory/ui/SettingsScreen.kt
+++ app/src/main/java/com/example/steelinventory/ui/SettingsScreen.kt
@@ -1,4 +1,4 @@
-package com.example.steelinventory.ui
+package com.example.steelinventory.ui.screens
 
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
